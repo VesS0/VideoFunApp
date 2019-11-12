271382017b8c56dd52d72c76960bb8ad9f8dad5a
Isidora Bojovic (E-Search) <v-isbojo@microsoft.com>
2019-11-12 13:22:46 +0100

    Our First Commit, made by ISIDORA hehe

---
+++ Transcription/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Transcription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
