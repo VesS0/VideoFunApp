948c827731e7f576793ca4be35a000ae98ac4b06
Isidora Bojovic (E-Search) <v-isbojo@microsoft.com>
2019-11-14 06:41:12 +0100

    translation

--- Transcription/CognitiveServices/Translation.cs
+++ Transcription/CognitiveServices/Translation.cs
@@ -1,10 +1,99 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Transcription
 {
-    class Translation
+    /// <summary>
+    /// The C# classes that represents the JSON returned by the Translator Text API.
+    /// </summary>
+    public class TranslationResult
     {
+        public DetectedLanguage DetectedLanguage { get; set; }
+        public TextResult SourceText { get; set; }
+        public Translation[] Translations { get; set; }
+    }
+
+    public class DetectedLanguage
+    {
+        public string Language { get; set; }
+        public float Score { get; set; }
+    }
+
+    public class TextResult
+    {
+        public string Text { get; set; }
+        public string Script { get; set; }
+    }
+
+    public class Translation
+    {
+        public string Text { get; set; }
+        public TextResult Transliteration { get; set; }
+        public string To { get; set; }
+        public Alignment Alignment { get; set; }
+        public SentenceLength SentLen { get; set; }
+    }
+
+    public class Alignment
+    {
+        public string Proj { get; set; }
+    }
+
+    public class SentenceLength
+    {
+        public int[] SrcSentLen { get; set; }
+        public int[] TransSentLen { get; set; }
+    }
+
+    public class TranslateText
+    {
+        private const string key_var = "40d19132f2224d82aa4fd3b947c11b80";
+        //private static readonly string subscriptionKey = Environment.GetEnvironmentVariable(key_var);
+
+        private const string endpoint_var = "https://api-eur.cognitive.microsofttranslator.com";
+        //private static readonly string endpoint = Environment.GetEnvironmentVariable(endpoint_var);
+
+        // This sample requires C# 7.1 or later for async/await.
+        // Async call to the Translator Text APIS
+        static public async Task TranslateTextRequest(
+            string route = "/translate?api-version=3.0&to=de&to=it&to=ja&to=th&to=sr-Latn", 
+            string inputText="Hello, My name is Isidora")
+        {
+            object[] body = new object[] { new { Text = inputText } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                // Build the request.
+                // Set the method to Post.
+                request.Method = HttpMethod.Post;
+                // Construct the URI and add headers.
+                request.RequestUri = new Uri(endpoint_var + route);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key_var);
+
+                // Send the request and get response.
+                HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                // Read response as a string.
+                string result = await response.Content.ReadAsStringAsync();
+                // Deserialize the response using the classes created earlier.
+                TranslationResult[] deserializedOutput = JsonConvert.DeserializeObject<TranslationResult[]>(result);
+                // Iterate over the deserialized results.
+                foreach (TranslationResult o in deserializedOutput)
+                {
+                    // Print the detected input language and confidence score.
+                    Console.WriteLine("Detected input language: {0}\nConfidence score: {1}\n", o.DetectedLanguage.Language, o.DetectedLanguage.Score);
+                    // Iterate over the results and print each translation.
+                    foreach (Translation t in o.Translations)
+                    {
+                        Console.WriteLine("Translated to {0}: {1}", t.To, t.Text);
+                    }
+                }
+            }
+        }
     }
 }
--- Transcription/Program.cs
+++ Transcription/Program.cs
@@ -7,14 +7,24 @@ namespace Transcription
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            var pathToVideo = @"C:\Users\v-isbojo\Pictures\OtherLangVideo\itit.mp4";
+            //var pathToVideo = @"C:\Users\v-isbojo\Pictures\OtherLangVideo\itit.mp4";
 
-            var video = Video.ImportVideo(pathToVideo);
+            //var video = Video.ImportVideo(pathToVideo);
 
-            var audio = ffmpeg.ExtractAudio(video);
+            //var audio = ffmpeg.ExtractAudio(video);
 
-            CognitiveServices.GetTranscript(audio);
+            //CognitiveServices.GetTranscript(audio);
 
+            try
+            {
+                TranslateText.TranslateTextRequest().Wait();
+            }
+            catch (Exception ex)
+            {
+                _ = ex;
+            }
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
             //var translation = CognitiveServices.GetTranslation(transcript, new[] { "en", "rs", "fr" });
         }
     }
