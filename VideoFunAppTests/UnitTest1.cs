6779294e4b0051e1a107c9992c69cf517454e760
Vladimir Vesic <vlvesic@microsoft.com>
2020-06-16 15:56:57 +0200

    Backup

---
+++ VideoFunAppTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VideoFunAppTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
